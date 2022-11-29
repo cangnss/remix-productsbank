71ebf44cd3033932c332b5104dcc687a98ece0ad
cangnss <cgunes52@gmail.com>
2022-11-29 23:54:51 +0300

    added category page

--- app/routes/admin/categories/new.tsx
+++ app/routes/admin/categories/new.tsx
@@ -1,7 +1,33 @@
-export default function AddCategory(){
+export default function AddCategory() {
     return (
-        <div>
-            add category
+      <div className="flex justify-center mx-auto flex-col bg-white drop-shadow-lg p-10 w-96">
+        <div className="text-center">
+          <p className="text-3xl">Add Category</p>
         </div>
-    )
-}
+        <div className="flex justify-center">
+          <form method="post">
+            <div className="flex flex-row p-3 w-100">
+              <label className="font-semibold my-auto mr-5" htmlFor="categoryName">
+                Category Name:
+              </label>
+              <input
+                type="text"
+                id="categoryName"
+                name="categoryName"
+                className="w-50 p-3 bg-slate-200 rounded-lg"
+              />
+            </div>
+            <div className="flex justify-end my-2 w-100">
+              <button
+                type="submit"
+                className="w-24 bg-cyan-700 text-white font-semibold p-2 rounded-lg"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  
