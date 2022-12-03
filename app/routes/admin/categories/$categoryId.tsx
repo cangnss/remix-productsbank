f4b06824637e7d1206805870044cf667171ff074
cangnss <cgunes52@gmail.com>
2022-12-03 13:33:58 +0300

    designed categories page

--- app/routes/admin/categories/$categoryId.tsx
+++ app/routes/admin/categories/$categoryId.tsx
@@ -1,6 +1,6 @@
 import { Category } from "@prisma/client"
 import { json } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import { Link, useLoaderData } from "@remix-run/react"
 import { LoaderFunction } from "@remix-run/server-runtime"
 import { db } from "~/utils/db.server"
 
@@ -20,16 +20,21 @@ export default function CategoryDetail(){
     const data = useLoaderData<LoaderData>();
     let i = 0;
     return(
-        <div>
-            <div className="flex flex-row text-white justify-between w-100 bg-blue-400 rounded-lg drop-shadow-lg">
-                <div className="p-10">
+        <div className="w-100 flex justify-center">
+            <div className="flex flex-row text-center text-white justify-between w-80 bg-blue-400 rounded-lg drop-shadow-lg">
+                <div className="p-5">
                     {++i}
                 </div>
-                <div className="p-10">
+                <div className="p-5">
                     {data?.category.id}
                 </div>
                 <div className="p-10">
                     {data?.category?.categoryName}
+                </div>
+                <div className="flex justify-center my-auto">
+                    <Link to={`/admin/products/asd`}>
+                        <button className="w-20 bg-white text-semibold text-black rounded-lg">Product detail</button>
+                    </Link>
                 </div>
             </div>
         </div>
--- app/routes/admin/categories/index.tsx
+++ app/routes/admin/categories/index.tsx
@@ -2,6 +2,7 @@ import { Category } from "@prisma/client";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData, Link } from "@remix-run/react";
 import { db } from "~/utils/db.server";
+import { BiCategory } from "react-icons/bi"
 
 type LoaderData = {
   categoriesItems: Array<Category>;
@@ -21,13 +22,16 @@ export default function CategoryIndex() {
 
   return (
     <div>
-      <div className="flex justify-center mx-auto flex-row bg-white drop-shadow-lg p-10 w-100">
+      <div className="flex flex-wrap justify-center mx-auto flex-row bg-white drop-shadow-lg p-10 w-100">
         {data.categoriesItems.map((category) => {
           return (
             <div
-              className="w-44 p-5 mr-5 font-semibold text-white text-center rounded-lg bg-cyan-600"
+              className="columns-3 mb-5 w-56 flex flex-col p-5 mr-5 font-semibold text-white text-center rounded-lg bg-cyan-600"
               key={category.id}
             >
+              <div className="text-center mx-auto">
+                <BiCategory className="text-3xl" />
+              </div>
               <Link to={`/admin/categories/${category.id}`}>
                 {category.categoryName}
               </Link>
